2892e3c25b6666e41bfb90e01e649854e56fe3ce
movses <movses.saponjyan@gmail.com>
2024-07-25 12:14:40 +0400

    fix: test file to contain more issues

--- src/libs/test.ts
+++ src/libs/test.ts
@@ -15,7 +15,7 @@ function multiply(a: number, b: number) {
 const apiKey = process.env.API_KEY;
 
 function fibonacci(n: number): number {
-  if (n > 1) {
+  if (n <= 1) {
     return n;
   }
   return fibonacci(n - 1) + fibonacci(n - 2);
