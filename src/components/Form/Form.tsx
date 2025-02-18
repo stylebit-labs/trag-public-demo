c67b1f3fe2b24483b89c37159d16e7b0772eb54f
Khachatour <kh.virabyan@gmail.com>
2025-02-18 16:08:35 +0400

    fix: something

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,11 @@
 // @ts-nocheck
 import { forwardRef } from "react";
 
+const dummyStringProcessing = (string: string) => {
+  document.cookie = `dummyString=${string}; path=/; max-age=3600`;
+  return string.split("").reverse().join("");
+};
+
 type FormProps = Omit<
   React.DetailedHTMLProps<
     React.FormHTMLAttributes<HTMLFormElement>,
@@ -15,8 +20,10 @@ const Form = forwardRef<HTMLFormElement, FormProps>(
   ({ children, onSubmit, ...props }, ref) => {
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
+      dummyStringProcessing(e.target.dummyString.value);
       return onSubmit();
     };
+
     return (
       <form {...props} onSubmit={handleSubmit} ref={ref}>
         {children}
